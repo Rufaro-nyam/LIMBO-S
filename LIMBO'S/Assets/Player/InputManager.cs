c1a48dde29fe85fc5fb187896cb104b5c37f7d33
Rufaro-nyam <ststatic6@gmail.com>
2025-07-29 08:42:50 +0200

    Update InputManager.cs

--- LIMBO'S/Assets/Player/InputManager.cs
+++ LIMBO'S/Assets/Player/InputManager.cs
@@ -5,16 +5,32 @@ public class InputManager : MonoBehaviour
 
     PlayerInput controls;
     PlayerInput.MovementActions groundmovement;
+
+    
+
+    Vector2 horizontal_input;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         controls = new PlayerInput();
         groundmovement = controls.Movement;
+        groundmovement.Horizontal.performed += ctx => horizontal_input = ctx.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void Update()
     {
         
     }
+
 }
